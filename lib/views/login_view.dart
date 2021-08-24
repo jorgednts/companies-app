75aa8a6d19fbb40adef9d0161b64e65bf14d0593
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-24 20:16:59 -0300

    add layout in _LoginViewState

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -8,23 +8,109 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  String _userEmail = '';
+  String _userPassword = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffebe9d7),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(padding: EdgeInsets.all(40),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: SizedBox(
             child: Container(
-                width: double.infinity,
-                child: Image.asset('assets/images/logo.png', width: 160, height: 50,)),)
-
-          ],
+                child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: 160,
+                  height: 50,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Text(
+                    'BEM VINDO AO \n EMPRESAS',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, contetur adipiscing elit. Nunc accumsan.',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: TextField(
+                    onChanged: (text) {
+                      _userEmail = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: TextField(
+                    onChanged: (text) {
+                      _userPassword = text;
+                    },
+                    keyboardType: TextInputType.number,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff57bbbc),
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      child: Text('ENTRAR', textAlign: TextAlign.center,),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            )),
+          ),
         ),
       ),
-
     );
   }
 }
