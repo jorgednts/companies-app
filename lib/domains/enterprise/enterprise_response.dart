741c7d95e242cbe0c148d96631d31c7cb914921e
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-25 15:07:36 -0300

    add methods gets in EnterpriseResponse

--- lib/domains/enterprise/enterprise_response.dart
+++ lib/domains/enterprise/enterprise_response.dart
@@ -1,9 +1,22 @@
 import 'package:ioasys_app/domains/enterprise/enterprise_type_response.dart';
 
 class EnterpriseResponse {
-  late String? enterprise_name;
-  late String? photo;
-  late String? description;
-  late String? country;
-  late EnterpriseTypeResponse? enterpriseTypeResponse;
+  late String? _enterprise_name;
+  late String? _photo;
+  late String? _description;
+  late String? _country;
+  late EnterpriseTypeResponse? _enterpriseTypeResponse;
+
+  EnterpriseResponse(this._enterprise_name, this._photo, this._description,
+      this._country, this._enterpriseTypeResponse);
+
+  EnterpriseTypeResponse? get enterpriseTypeResponse => _enterpriseTypeResponse;
+
+  String? get country => _country;
+
+  String? get description => _description;
+
+  String? get photo => _photo;
+
+  String? get enterprise_name => _enterprise_name;
 }
