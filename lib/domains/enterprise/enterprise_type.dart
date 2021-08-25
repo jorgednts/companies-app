eaf8ad72c94c7d9c27c77f05cd5a23b8fdfe0151
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-25 15:08:43 -0300

    add method get in EnterpriseType

--- lib/domains/enterprise/enterprise_type.dart
+++ lib/domains/enterprise/enterprise_type.dart
@@ -1,3 +1,7 @@
 class EnterpriseType{
-  late String enterprise_type_name;
+  late String _enterprise_type_name;
+
+  EnterpriseType(this._enterprise_type_name);
+
+  String get enterprise_type_name => _enterprise_type_name;
 }
