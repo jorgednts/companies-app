56c4faa7853195f752d7546db309b8462fab6443
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-25 14:54:44 -0300

    create class Enterprise

--- lib/domains/enterprise/enterprise.dart
+++ lib/domains/enterprise/enterprise.dart
@@ -1,3 +1,9 @@
-class Enterprise {
+import 'package:ioasys_app/domains/enterprise/enterprise_type.dart';
 
+class Enterprise {
+  late String enterprise_name;
+  late String photo;
+  late String description;
+  late String country;
+  late EnterpriseType enterprise_type;
 }
