9aed115640292b5530eaf4b633978cfe13697bcf
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-25 15:27:41 -0300

    add function validateUserEmail in class User

--- lib/domains/user/user.dart
+++ lib/domains/user/user.dart
@@ -9,4 +9,17 @@ class User {
   String get userPassword => _userPassword;
 
   String get userEmail => _userEmail;
+
+  EmailStatus validateUserEmail(String email) {
+    RegExp regex = RegExp(
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\'
+        r'[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.'
+        r')+[a-zA-Z]{2,}))$');
+    if (!regex.hasMatch(email))
+      return EmailStatus.INVALID;
+    else if (email.isEmpty)
+      return EmailStatus.EMPTY;
+    else
+      return EmailStatus.VALID;
+  }
 }
