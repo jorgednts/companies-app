bd9ff8584c481cce53411335147dcc2c7e5e1c54
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-10-18 19:30:11 -0300

    create package data

---
+++ lib/data/remote/enterprise/repository/enterprise_repository.dart
@@ -0,0 +1,5 @@
+import 'package:ioasys_app/data/remote/enterprise/repository/enterprise_data_repository.dart';
+
+class EnterpriseRepository implements EnterpriseDataRepository {
+
+}
---
+++ lib/data/remote/user/repository/user_repository.dart
@@ -0,0 +1,5 @@
+import 'package:ioasys_app/data/remote/user/repository/user_data_repository.dart';
+
+class UserRepository implements UserDataRepository {
+
+}
