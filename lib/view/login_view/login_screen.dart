392bc3b7c7f783e25ef9824f8274270e3b43e700
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-11-04 09:54:17 -0300

    add formKey in LoginScreen

--- lib/view/login_view/login_screen.dart
+++ lib/view/login_view/login_screen.dart
@@ -22,143 +22,146 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) => Scaffold(
         backgroundColor: const Color(0xffebe9d7),
-        body: SizedBox.expand (
+        body: SizedBox.expand(
           child: SingleChildScrollView(
             child: Column(
               children: [
-            const SizedBox(
-              height: 30,
-            ),
-            Image.asset(
-              ConstantsImages.imageLogo,
-              width: 160,
-              height: 50,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 20, left: 20),
-              child: Text(
-                S.of(context).loginScreenWelcomeText.toUpperCase(),
-                style: const TextStyle(
-                  fontSize: 20,
+                const SizedBox(
+                  height: 30,
                 ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 20, left: 20),
-              child: Text(
-                S.of(context).loginScreenIntroductionText,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 20, left: 20),
-              child: Form(
-                key: _formKeyEmail,
-                child: TextFormField(
-                  controller: _userEmailInputController,
-                  validator: (userEmail) {
-                    if (UserModel.validateUserEmail(userEmail) ==
-                        EmailStatus.invalid) {
-                      return S.of(context).loginScreenFormInvalidEmail;
-                    } else if (UserModel.validateUserEmail(userEmail) ==
-                        EmailStatus.empty) {
-                      return S.of(context).loginScreenEmptyFormText;
-                    } else {
-                      return null;
-                    }
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText:
-                        S.of(context).loginScreenFormEmailLabelText,
-                    labelStyle: const TextStyle(color: Colors.grey),
-                    prefixIcon: const Icon(
-                      Icons.email,
-                      color: Color(0xffee4c77),
-                    ),
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey),
-                    ),
-                    enabledBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey),
-                    ),
-                  ),
+                Image.asset(
+                  ConstantsImages.imageLogo,
+                  width: 160,
+                  height: 50,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 20, left: 20),
-              child: Form(
-                key: _formKeyPassword,
-                child: TextFormField(
-                  controller: _userPasswordInputController,
-                  validator: (userPassword) {
-                    if (userPassword == null) {
-                      return S.of(context).loginScreenEmptyFormText;
-                    } else if (userPassword.length < 8) {
-                      return S.of(context).loginScreenFormInvalidPassword;
-                    } else {
-                      return null;
-                    }
-                  },
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText:
-                        S.of(context).loginScreenFormPasswordLabelText,
-                    labelStyle: const TextStyle(color: Colors.grey),
-                    prefixIcon: const Icon(
-                      Icons.password,
-                      color: Color(0xffee4c77),
-                    ),
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey),
-                    ),
-                    enabledBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey),
-                    ),
-                  ),
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 20, left: 20),
-              child: ElevatedButton(
-                onPressed: () {
-                  _formKeyEmail.currentState!.validate();
-                  _formKeyPassword.currentState!.validate();
-                  Navigator.of(context).pushReplacementNamed('/main');
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: const Color(0xff57bbbc),
-                ),
-                child: Container(
-                  width: double.infinity,
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
                   child: Text(
-                    S.of(context).loginScreenButtonText,
+                    S.of(context).loginScreenWelcomeText.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 20,
+                    ),
                     textAlign: TextAlign.center,
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Text(
+                    S.of(context).loginScreenIntroductionText,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Form(
+                    key: _formKeyEmail,
+                    child: TextFormField(
+                      controller: _userEmailInputController,
+                      validator: (userEmail) {
+                        if (UserModel.validateUserEmail(userEmail) ==
+                            EmailStatus.invalid) {
+                          return S.of(context).loginScreenFormInvalidEmail;
+                        } else if (UserModel.validateUserEmail(userEmail) ==
+                            EmailStatus.empty) {
+                          return S.of(context).loginScreenEmptyFormText;
+                        } else {
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: S.of(context).loginScreenFormEmailLabelText,
+                        labelStyle: const TextStyle(color: Colors.grey),
+                        prefixIcon: const Icon(
+                          Icons.email,
+                          color: Color(0xffee4c77),
+                        ),
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Form(
+                    key: _formKeyPassword,
+                    child: TextFormField(
+                      controller: _userPasswordInputController,
+                      validator: (userPassword) {
+                        if (userPassword == null) {
+                          return S.of(context).loginScreenEmptyFormText;
+                        } else if (userPassword.length < 8) {
+                          return S.of(context).loginScreenFormInvalidPassword;
+                        } else {
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText:
+                            S.of(context).loginScreenFormPasswordLabelText,
+                        labelStyle: const TextStyle(color: Colors.grey),
+                        prefixIcon: const Icon(
+                          Icons.password,
+                          color: Color(0xffee4c77),
+                        ),
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final isValidEmail =
+                          _formKeyEmail.currentState?.validate() ?? false;
+                      final isValidPassword =
+                          _formKeyPassword.currentState?.validate() ?? false;
+                      if(isValidEmail && isValidPassword) {
+                        Navigator.of(context).pushReplacementNamed('/main');
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff57bbbc),
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      child: Text(
+                        S.of(context).loginScreenButtonText,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
               ],
             ),
           ),
