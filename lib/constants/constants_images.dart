ec9a1f42f06015ad511bbea69995d0485cbf095a
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-11-08 03:00:08 -0300

    add assets image

--- lib/constants/constants_images.dart
+++ lib/constants/constants_images.dart
@@ -1,4 +1,6 @@
 class ConstantsImages {
   static const String imageLogo = 'assets/images/logo.png';
   static const String imageLogoInAppBar = 'assets/images/logo_nav.png';
+  static const String imageLoading = 'assets/images/loading.png';
+  static const String imageError = 'assets/images/error.png';
 }
--- lib/data/remote/enterprise/remote_data_source/enterprise_remote_data_source.dart
+++ lib/data/remote/enterprise/remote_data_source/enterprise_remote_data_source.dart
@@ -1,3 +1,4 @@
+
 import 'package:dio/dio.dart';
 import 'package:ioasys_app/data/remote/enterprise/model/enterprise/enterprise_list_response.dart';
 import 'package:ioasys_app/data/remote/shared/exception/gerenic_error_status_code_exception.dart';
@@ -13,16 +14,17 @@ class EnterpriseRemoteDataSource {
   Future<EnterpriseListResponse> getEnterpriseList(String enterpriseName,
       String accessToken, String uid, String client) async {
     try {
-      final response =
-          await _dio.get('${_baseUrl}enterprises?name=$enterpriseName',
-              queryParameters: {'name': enterpriseName},
-              options: Options(
-                headers: {
-                  'access-token': accessToken,
-                  'client': client,
-                  'uid': uid,
-                },
-              ));
+      final response = await _dio.get(
+        '${_baseUrl}enterprises',
+        queryParameters: {'name': enterpriseName},
+        options: Options(
+          headers: {
+            'access-token': accessToken,
+            'client': client,
+            'uid': uid,
+          },
+        ),
+      );
       return EnterpriseListResponse.fromJson(response.data);
     } on DioError catch (dioError, _) {
       if (dioError.type == DioErrorType.response) {
