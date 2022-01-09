aa6b9fbc787717760ec31a5c6c4c2c165302554f
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-01-09 18:47:24 -0300

    create enterprise_remote_data_source_impl_test.dart

---
+++ test/external/remote_data_source/enterprise/enterprise_remote_data_source_impl_test.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:ioasys_app/external/remote_data_source/enterprise/enterprise_remote_data_source_impl.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'enterprise_remote_data_source_impl_test.mocks.dart';
+
+@GenerateMocks([Dio])
+void main() {
+  late MockDio mockDio;
+  late EnterpriseRemoteDataSourceImpl enterpriseRemoteDataSourceImpl;
+  const baseUrl = 'https://empresas.ioasys.com.br/api/v1/';
+  setUpAll(() {
+    mockDio = MockDio();
+    enterpriseRemoteDataSourceImpl = EnterpriseRemoteDataSourceImpl(mockDio);
+  });
+  setUp(() {
+    reset(mockDio);
+  });
+  group('given a call on getEnterpriseList', () {});
+  group('given a call on getEnterprise', () {});
+}
