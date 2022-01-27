8b3b612934c50279bd8252a9fc34f56a7fbc69b0
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-01-27 16:35:23 -0300

    create validate_email_use_case_test.dart

--- test/domain/use_case/validate_email_use_case_test.dart
+++ test/domain/use_case/validate_email_use_case_test.dart
@@ -1,1 +1,28 @@
-void main() {}
+import 'package:ioasys_app/domain/model/user/email_status.dart';
+import 'package:ioasys_app/domain/use_case/validate_email_use_case.dart';
+
+import 'package:test/test.dart';
+
+void main() {
+  late ValidateEmailUseCase validateEmailUseCase;
+  setUpAll(() {
+    validateEmailUseCase = ValidateEmailUseCase();
+  });
+  group('GIVEN a call on validateEmail', () {
+    test('WHEN email is empty THEN it should return an EmailStatus.empty',
+        () async {
+      final emailStatus = validateEmailUseCase.validateEmail('');
+      expect(emailStatus, EmailStatus.empty);
+    });
+    test('WHEN email is invalid THEN it should return an EmailStatus.invalid',
+        () async {
+      final emailStatus = validateEmailUseCase.validateEmail('email');
+      expect(emailStatus, EmailStatus.invalid);
+    });
+    test('WHEN email is valid THEN it should return an EmailStatus.valid',
+        () async {
+      final emailStatus = validateEmailUseCase.validateEmail('email@email.com');
+      expect(emailStatus, EmailStatus.valid);
+    });
+  });
+}
--- test/domain/use_case/validate_password_use_case_test.dart
+++ test/domain/use_case/validate_password_use_case_test.dart
@@ -1,1 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
 void main() {}
