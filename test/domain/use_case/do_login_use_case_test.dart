e932a011f630c36408fcd6c8f101f4135b268c27
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-01-27 16:21:02 -0300

    create do_login_use_case_test.dart and get_enterprise_list_use_case_test.dart

--- test/domain/use_case/do_login_use_case_test.dart
+++ test/domain/use_case/do_login_use_case_test.dart
@@ -1,7 +1,46 @@
+import 'package:ioasys_app/domain/model/user/user_model.dart';
+import 'package:ioasys_app/domain/model/user/user_tokens.dart';
 import 'package:ioasys_app/domain/repository/user/user_repository.dart';
+import 'package:ioasys_app/domain/use_case/do_login_use_case.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'do_login_use_case_test.mocks.dart';
+
 @GenerateMocks([UserRepository])
-void main() {}
+void main() {
+  late MockUserRepository mockUserRepository;
+  late DoLoginUseCase doLoginUseCase;
+  setUpAll(() {
+    mockUserRepository = MockUserRepository();
+    doLoginUseCase = DoLoginUseCase(mockUserRepository);
+  });
+  setUp(() {
+    reset(mockUserRepository);
+  });
+  group('GIVEN a call on getRawFuture', () {
+    test(
+        'WHEN request is successfully '
+        'THEN it should return an UserTokens', () async {
+      when(mockUserRepository.doLogin(_getSuccessfulUserModelMock()))
+          .thenAnswer((_) async => _getSuccessfulUserTokensMock());
+      final userTokens = await doLoginUseCase.getRawFuture(
+          params: DoLoginUseCaseParams(_getSuccessfulUserModelMock()));
+      expect(userTokens, equals(_getSuccessfulUserTokensMock()));
+      verify(mockUserRepository.doLogin(_getSuccessfulUserModelMock()))
+          .called(1);
+    });
+  });
+}
+
+UserTokens _getSuccessfulUserTokensMock() => const UserTokens(
+      '1234',
+      '1145',
+      '1253',
+    );
+
+UserModel _getSuccessfulUserModelMock() => const UserModel(
+      'testeapple@ioasys.com.br',
+      '12341234',
+    );
--- test/domain/use_case/get_enterprise_list_use_case_test.dart
+++ test/domain/use_case/get_enterprise_list_use_case_test.dart
@@ -1,7 +1,59 @@
+import 'package:ioasys_app/constants/constants_url_api.dart';
+import 'package:ioasys_app/domain/model/enterprise/enterprise_model.dart';
+import 'package:ioasys_app/domain/model/enterprise/enterprise_type_model.dart';
 import 'package:ioasys_app/domain/repository/enterprise/enterprise_repository.dart';
+import 'package:ioasys_app/domain/use_case/get_enterprise_list_use_case.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'get_enterprise_list_use_case_test.mocks.dart';
+
 @GenerateMocks([EnterpriseRepository])
-void main() {}
+void main() {
+  late MockEnterpriseRepository mockEnterpriseRepository;
+  late GetEnterpriseListUseCase getEnterpriseListUseCase;
+  setUpAll(() {
+    mockEnterpriseRepository = MockEnterpriseRepository();
+    getEnterpriseListUseCase =
+        GetEnterpriseListUseCase(mockEnterpriseRepository);
+  });
+  setUp(() {
+    reset(mockEnterpriseRepository);
+  });
+  group('GIVEN a call on getRawFuture', () {
+    test(
+        'WHEN request is successfully '
+        'THEN it should return an EnterpriseModel list', () async {
+      when(mockEnterpriseRepository.getEnterpriseList(
+              'am', 'accessToken', 'uid', 'client'))
+          .thenAnswer((_) async => _getSuccessfulEnterpriseListModelMock());
+      final enterpriseList = await getEnterpriseListUseCase.getRawFuture(
+          params: GetEnterpriseListUseCaseParams(
+              'am', 'accessToken', 'uid', 'client'));
+      expect(enterpriseList, equals(_getSuccessfulEnterpriseListModelMock()));
+      verify(mockEnterpriseRepository.getEnterpriseList(
+          'am', 'accessToken', 'uid', 'client'));
+    });
+  });
+}
+
+List<EnterpriseModel> _getSuccessfulEnterpriseListModelMock() =>
+    <EnterpriseModel>[
+      const EnterpriseModel(
+        'TeamPlayerHR',
+        '${ConstantsUrlApi.imageBaseUrl}/uploads/enterprise/photo/16/240.jpeg',
+        'description',
+        'UK',
+        EnterpriseTypeModel('HR Tech'),
+        16,
+      ),
+      const EnterpriseModel(
+        'Advanced Diamond X',
+        '${ConstantsUrlApi.imageBaseUrl}/uploads/enterprise/photo/35/240.jpeg',
+        'description',
+        'UK',
+        EnterpriseTypeModel('Industry'),
+        35,
+      ),
+    ];
